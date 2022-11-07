64dc93bd6f5ca66dfe3742bb5a7a9d975f3b0b98
ybahae <ybahaerarslan@gmail.com>
2022-11-07 01:20:03 +0100

    init

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  assetPrefix: process.env.NODE_ENV === "production" ? "/" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
