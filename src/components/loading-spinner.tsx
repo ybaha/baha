89db0b7a83cf067cea78565da618361d5ec9c144
ybahae <ybahaerarslan@gmail.com>
2024-12-30 23:34:02 +0100

    fix attempt

--- src/components/loading-spinner.tsx
+++ src/components/loading-spinner.tsx
@@ -2,7 +2,7 @@ import { Loader } from "lucide-react";
 
 export const LoadingSpinner = () => {
   return (
-    <div className="flex items-center justify-center h-full w-full">
+    <div className="flex items-center justify-center h-full w-full min-h-[60vh]">
       <Loader className="h-6 w-6 animate-spin text-foreground/80" />
     </div>
   );
