c983159eab13ebfa4485937bab8dba84bd51f3c0
ybaha <ybahaerarslan@gmail.com>
2025-10-29 21:59:35 +0100

    changes on blog

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -1,49 +1,46 @@
-import Link from "@/components/link";
-import { useMDXComponent } from "next-contentlayer2/hooks";
-import { cn } from "@/lib/utils";
+import Link from '@/components/link';
+import { useMDXComponent } from 'next-contentlayer2/hooks';
+import { cn } from '@/lib/utils';
+import { cloneElement } from 'react';
 
 const mdxComponents = {
-  Image: (props: any) => (
-    <img className={cn(props.className, "rounded-lg")} {...props} />
-  ),
+  Image: (props: any) => <img className={cn(props.className, 'rounded-lg')} {...props} />,
   Link: (props: any) => <Link {...props} />,
   ul: (props: any) => <ul {...props} className="mb-8 list-disc pl-7" />,
   ol: (props: any) => <ol {...props} className="mb-8 list-decimal pl-6" />,
-  h2: (props: any) => (
-    <h2 {...props} className={cn(props.className, "mb-5 lg:mb-5")} />
-  ),
-  h3: (props: any) => (
-    <h3 {...props} className={cn(props.className, "mb-4 lg:mb-5")} />
-  ),
-  h4: (props: any) => (
-    <h4 {...props} className={cn(props.className, "mb-3 lg:mb-4")} />
-  ),
-  p: (props: any) => (
-    <p {...props} className="text-sm sm:text-base mb-4 lg:mb-6" />
-  ),
+  h2: (props: any) => <h2 {...props} className={cn(props.className, 'mb-5 lg:mb-5')} />,
+  h3: (props: any) => <h3 {...props} className={cn(props.className, 'mb-4 lg:mb-5')} />,
+  h4: (props: any) => <h4 {...props} className={cn(props.className, 'mb-3 lg:mb-4')} />,
+  p: (props: any) => <p {...props} className="text-sm sm:text-base mb-4 lg:mb-6" />,
   pre: ({ children, className, ...props }: any) => {
-    const hasLang = /language-(\w+)/.exec(className || "");
+    const hasLang = /language-(\w+)/.exec(className || '');
+
+    // react.cloneElement(children, { className: cn(children.props.className, 'bg-transparent') });
+    const clonedChildren = cloneElement(children, {
+      className: cn(children.props.className, 'bg-transparent'),
+    });
+
     return (
       <pre
         className={cn(
-          "border rounded-lg font-medium dark:bg-zinc-900 dark:border-border p-4 mb-4 overflow-x-auto relative",
-          className
+          'border rounded-lg font-medium dark:bg-zinc-900 dark:border-border p-4 mb-4 overflow-x-auto relative',
+          className,
         )}
         {...props}
       >
         {hasLang && (
-          <div className="absolute right-4 top-4 text-xs text-gray-400">
-            {hasLang[1]}
-          </div>
+          <div className="absolute right-4 top-4 text-xs text-gray-400">{hasLang[1]}</div>
         )}
-        {children}
+        {clonedChildren}
       </pre>
     );
   },
   code: ({ className, children, ...props }: any) => {
-    const hasLang = /language-(\w+)/.exec(className || "");
     return (
-      <code className={cn("font-mono text-sm", className)} {...props}>
+      <code
+        className={cn('font-mono text-sm bg-[#f0f0f0] dark:bg-zinc-900 p-1 rounded-md', className)}
+        {...props}
+      >
         {children}
       </code>
     );
@@ -59,13 +56,7 @@ interface MdxProps {
 export function Mdx({ code, journey, className }: MdxProps) {
   const MDXContent = useMDXComponent(code);
   return (
-    <div
-      className={cn(
-        "dark:text-gray-300 text-gray-800",
-        journey && "text-sm",
-        className
-      )}
-    >
+    <div className={cn('dark:text-gray-300 text-gray-800', journey && 'text-sm', className)}>
       <MDXContent components={mdxComponents} />
     </div>
   );
